39601526f34fccd0787a93be0fff2cc64c7d3d72
KevinDaLam <kvnlam23@gmail.com>
2017-11-23 00:02:39 -0500

    Peripherals and Main Tasks revamp

--- main.c
+++ main.c
@@ -1,73 +1,146 @@
 #include <RTL.h>
+#include <lpc17xx.h>
+#include <stdio.h>
 #include <stdbool.h>
 #include <stdint.h>
-#include <peripherals.h>
+#include "GLCD.h"
+#include "peripherals.h"
+#include "avatar.h"
+#include "platform.h"
 
+#define PLATFORM_INTERVAL_SIZE	60
+#define MAX_N_PLATFORMS			5
+#define SCREEN_HEIGHT			300
+#define AVATAR_SPEED			2
+#define TERMINAL_VELOCITY		4
+
+uint32_t score_count = 0;
 uint8_t n_lives = 3;
 uint8_t n_powerups = 3;
+uint8_t current_platform = MAX_N_PLATFORMS/2;
 
-struct Avatar avatar1 = DEFAULT_AVATAR;
+struct Avatar *avatar1;
+bool avatar_orient_r = true;
+uint8_t fall_step = 1;
+
+struct Platform *platforms[MAX_N_PLATFORMS];
+uint8_t platform_speed = 1;
+
+OS_MUT avatar_mut;
+
 
 bool cmd_powerup = false;
 
 __task void update_avatar(void){
-  
-  uint8_t command;
-  
-  while(true){
+	
+	uint8_t command;
+
+	while(true){
   	
-  	os_tsk_pass();
-  	
-  	command = poll_joystick_direction();
-  	
-  	if (command == CMD_MOVE_LEFT){
-  		avatar1->x_pos--
-  	}
-  	else if (command == CMD_MOVE_RIGHT){
-		avatar1->x_pos++
-  	}
-  	
-  	if (avatar_check_hole(avatar1)){
-  		avatar_start_fall(avatar1);
-  	}
-  	
-  	if (avatar1->falling){
-  		
-  		if(avatar->y_pos_falling > avatar->platform->y_pos){
-  			avatar_fall_decrement_y(avatar1);
-  		}
-  		else{
-  			avatar1->falling = false;
-  		}
-  		
-  	}
-  	
-  }
-  
+		os_tsk_pass();
+		
+		os_mut_wait(&avatar_mut, 0xffff);
+		
+		command = poll_joystick_direction();
+
+		if (command == CMD_MOVE_LEFT){
+			avatar1->x_pos = avatar1->x_pos <= 0 ? 0 : (avatar1->x_pos >= AVATAR_SPEED ? avatar1->x_pos - AVATAR_SPEED : 0);
+			avatar_orient_r = false;
+		}
+		else if (command == CMD_MOVE_RIGHT){
+			avatar1->x_pos = avatar1->x_pos >= SCREEN_WIDTH - avatar1->size ? SCREEN_WIDTH - avatar1->size  : avatar1->x_pos + AVATAR_SPEED;
+			avatar_orient_r = true;
+		}
+
+		
+		if (avatar1->falling){
+			
+			GLCD_clear_avatar(avatar1);
+			
+			if(avatar1->y_pos_falling > avatar1->platform->y_pos){
+				avatar_fall_decrement_y(avatar1, fall_step);
+				fall_step = fall_step == TERMINAL_VELOCITY ? TERMINAL_VELOCITY : fall_step + 1;
+			}
+			else{
+				avatar1->falling = false;
+				score_count++;
+				if (!avatar_check_hole(avatar1)){
+					fall_step = 0;
+				}
+			}
+		}
+		else{
+			if(avatar_check_hole(avatar1)){
+				current_platform = (current_platform + 1) % MAX_N_PLATFORMS;
+				avatar_start_fall(avatar1, platforms[current_platform]);
+			}
+		}
+		
+		os_mut_release(&avatar_mut);
+		
+	}
 }
 
 __task void update_platforms(void){
-  
-  while(true){
-  	
-  	os_tsk_pass();
-  	
-  	if (cmd_powerup && n_powerups){
-  		platform_dig(avatar1->platform, avatar1->x_pos);
-  		cmd_powerup = false;
-  	}
 	
-  }
+	uint8_t highest_platform = 0;
+	
+	while(true){
+		
+		int i;
+		
+		os_tsk_pass();
+		
+		os_mut_wait(&avatar_mut, 0xffff);
+		
+		for(i = 0; i < MAX_N_PLATFORMS; i++){
+			/*
+			if((platforms[i == 0 ? MAX_N_PLATFORMS-1:i-1]->y_pos >= platforms[i]->y_pos + PLATFORM_INTERVAL_SIZE) || i != highest_platform){
+				platform_refresh(platforms[i], platform_speed);
+			}
+			*/
+		
+			if (platforms[highest_platform]->y_pos >= SCREEN_HEIGHT){
+				platform_recycle(platforms[highest_platform]);
+				highest_platform = (highest_platform + 1) % MAX_N_PLATFORMS;
+			}
+		
+			if (cmd_powerup && n_powerups){
+				printf("HELLO");
+				platform_dig(avatar1->platform, avatar1->x_pos);
+				cmd_powerup = false;
+				n_powerups--;
+			}
+	
+		}
+		
+		os_mut_release(&avatar_mut);
+	}
 }
 
 __task void update_display(void){
-  
-  while(true){
-  	
-  	os_tsk_pass();
-  	
-  }
-  
+	
+	while(true){
+
+		int i, j;
+		
+		os_tsk_pass();
+		
+		os_mut_wait(&avatar_mut, 0xffff);
+		
+		GLCD_update_avatar(avatar1, avatar_orient_r);
+		
+		for (j = 0; j < 100000; j++);
+		
+		for(i = 0; i < MAX_N_PLATFORMS; i++){
+			platform_refresh(platforms[i], platform_speed);
+			GLCD_update_platform(platforms[i], platform_speed);
+		}
+		
+		os_mut_release(&avatar_mut);
+
+	}
+
 }
 
 __task void start_tasks(void){
@@ -75,6 +148,8 @@ __task void start_tasks(void){
 	os_tsk_create(update_avatar, 1);
 	os_tsk_create(update_platforms, 1);
 	os_tsk_create(update_display, 1);
+	
+	os_mut_init(&avatar_mut);
 	
 	os_tsk_delete_self();
 	
@@ -85,14 +160,53 @@ void EINT3_IRQHandler(void){
 	LPC_GPIOINT->IO2IntClr |= (1<<10);
 }
 
+void platforms_setup(void){
+	
+	int i;
+	
+	for(i = 0; i < MAX_N_PLATFORMS; i++){
+		platforms[i] = platform_spawn();
+		platforms[i]->y_pos = PLATFORM_INTERVAL_SIZE*(MAX_N_PLATFORMS - 1 - i);
+	}
+
+}
+
+void avatar_setup(void){
+	avatar1 = avatar_init();
+	
+	avatar1->platform = platforms[current_platform];
+	avatar1->x_pos = SCREEN_WIDTH/2;
+}
+
 int main(void){
+	
+	platforms_setup();
+	avatar_setup();
+	
+	//Powerup Button Initialization
+	LPC_PINCON->PINSEL4 &= ~(3 << 20);
+	LPC_GPIO2->FIODIR &= ~(1<<10);
+	LPC_GPIOINT->IO2IntEnF |= (1<<10);
   
-  LPC_PINCON->PINSEL4 &= ~(3 << 20);
-  LPC_GPIO2->FIODIR &= ~(1<<10);
-  LPC_GPIOINT->IO2IntEnF |= (1<<10);
-  
-  NVIC_EnableIRQ(ENT3_IRQn);
-  
-  os_sys_init(start_tasks);
+	//LED Initialization
+	LPC_GPIO1->FIODIR |= 0xB0000000;
+	LPC_GPIO2->FIODIR |= 0x7C;
+	
+	LPC_GPIO1->FIOCLR |= 0xFF000000;
+	LPC_GPIO2->FIOCLR |= 0xFF;
+	
+	//Display Initialization
+	GLCD_Init();
+	GLCD_Clear(White);
+	GLCD_SetBackColor(White);
+	
+	
+	
+	printf("STARTING GAME\n");
+	
+	NVIC_EnableIRQ( EINT3_IRQn );
+	
+
+	os_sys_init(start_tasks);
   
 }
--- peripherals.c
+++ peripherals.c
@@ -1,9 +1,9 @@
-#include "led.h"
 #include "peripherals.h"
+#include "bitmaps.h"
 
-uint8_t poll_joystick_direction(){
+uint8_t poll_joystick_direction(void){
 	
-	GPIO1_state = LPC_GPIO1->FIOPIN;
+	uint32_t GPIO1_state = LPC_GPIO1->FIOPIN;
   	
 	if (~(GPIO1_state) & JS_LEFT){
 		return CMD_MOVE_LEFT;
@@ -15,4 +15,84 @@ uint8_t poll_joystick_direction(){
 		return CMD_NO_MOVE;
 	}
 	
+}
+
+void GLCD_update_avatar(struct Avatar *avatar, bool orientation_right){
+	
+	if (avatar->falling){
+
+		if(orientation_right){
+			GLCD_Bitmap(avatar->y_pos_falling + 2, avatar->x_pos, ESCAPERMAN_X, ESCAPERMAN_Y, (unsigned char*)ESCAPERMAN_BITMAP_R);
+		}
+		else{
+			GLCD_Bitmap(avatar->y_pos_falling + 2, avatar->x_pos, ESCAPERMAN_X, ESCAPERMAN_Y, (unsigned char*)ESCAPERMAN_BITMAP_L);
+		}
+	}
+	else{
+		if(orientation_right){
+			GLCD_Bitmap(avatar->platform->y_pos + 2, avatar->x_pos, ESCAPERMAN_X, ESCAPERMAN_Y, (unsigned char*)ESCAPERMAN_BITMAP_R);
+		}
+		else{
+			GLCD_Bitmap(avatar->platform->y_pos + 2, avatar->x_pos, ESCAPERMAN_X, ESCAPERMAN_Y, (unsigned char*)ESCAPERMAN_BITMAP_L);
+		}
+	}
+	
+}
+
+void GLCD_update_platform(struct Platform *platform, int refresh_step) {
+	int i = 0, x_loc = 0, x_end = -1;
+	int y_loc = platform->y_pos - refresh_step;
+	int hole_locs[HOLE_MAX_N + 1];
+
+	// Parse for hole locations
+	for (i = 0; i < HOLE_MAX_N; i++) {
+		if (platform->holes[i].size > 0)
+			hole_locs[i] = platform->holes[i].loc;
+		else
+			hole_locs[i] = -1;
+	}
+	// Last 'hole' is the end of the platform
+	hole_locs[HOLE_MAX_N] = PLATFORM_WIDTH;
+	
+	//printf("\nPlatform specs:: y_pos: %d, hole sizes: %d %d %d, hole locs: %d %d %d %d\n", platform->y_pos, platform->holes[0].size, platform->holes[1].size, platform->holes[2].size, 
+	//																									 hole_locs[0], hole_locs[1], hole_locs[2], hole_locs[3]);	
+	
+	// Setting color to erase platform
+	GLCD_SetTextColor(BACKGROUND_COLOR);	
+	for (i = 0; i < HOLE_MAX_N + 1; i++) {
+		// Check if hole is present
+		if (hole_locs[i] > -1) {
+			x_loc = hole_locs[i];
+			while (x_loc > x_end) {
+				// We use reversed x-y axis for the LCD
+				GLCD_PutPixel(y_loc, x_loc);
+				x_loc--;
+			}
+			x_end = hole_locs[i] + HOLE_MAX_SIZE;
+		}
+	}
+	
+	y_loc += refresh_step;
+	x_end = -1;
+	
+	// Setting color to draw platform
+	GLCD_SetTextColor(PLATFORM_COLOR);	
+	for (i = 0; i < HOLE_MAX_N + 1; i++) {
+		// Check if hole is present
+		if (hole_locs[i] > -1) {
+			x_loc = hole_locs[i];			
+			while (x_loc > x_end) {
+				// We use reversed x-y axis for the LCD
+				GLCD_PutPixel(y_loc, x_loc);
+				//printf("\n X_LOC: %d\n", x_loc);
+				x_loc--;
+			}
+			x_end = hole_locs[i] + HOLE_MAX_SIZE;
+			//printf("\n X_END: %d\n", x_end);
+		}
+	}		
+}
+
+void GLCD_clear_avatar(struct Avatar *avatar){
+	GLCD_Bitmap(avatar->y_pos_falling, avatar->x_pos, ESCAPERMAN_X+2, ESCAPERMAN_Y, (unsigned char*)ESCAPERMAN_CLEAR);
 }
